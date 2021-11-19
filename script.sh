579542369e38edd969a8c8805efcf2ae085a19e1
sternmate2 <shaharstern@hotmail.com>
2021-11-19 17:00:37 +0200

    fin hopefuly..

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/sh/
 
 sudo apt-get update -y
 sudo apt-get install \
@@ -7,7 +7,7 @@ sudo apt-get install \
     gnupg \
     lsb-release
     
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
